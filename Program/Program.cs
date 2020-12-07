386ca8fffb5c5a9bd657cf1803530f55c1d3ae67
Dominik Widlak <dominik.widlak@microsoft.wsei.edu.pl>
2020-12-07 23:21:17 +0100

    removeProgram

--- Program/Program.cs
+++ Program/Program.cs
@@ -7,15 +7,6 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Time t1 = new Time(1,5);
-            Time t = new Time(21,6,0);
-            TimePeriod tP = new TimePeriod(t,t1);
-            TimePeriod tP2 = new TimePeriod("2:3:5");
-            System.Console.WriteLine(tP.seconds);
-            System.Console.WriteLine(tP);
-            System.Console.WriteLine(t.CompareTo(t1));
-            Console.WriteLine(t);
-            System.Console.WriteLine(t-t1);
         }
     }
 }
